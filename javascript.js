8585fd1de3a6a29232e4a767ff9a3d95770cda3e
Muhammad Rehan <rehanbhaifromsdk@gmail.com>
2025-08-02 22:13:15 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+console.log("Educational Qualifications in Pakistan:");
+for (var i = 0; i < qualifications.length; i++) {
+  console.log((i + 1) + ". " + qualifications[i]);
+}
